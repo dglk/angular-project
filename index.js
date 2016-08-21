4587693658b822c27e7d2cab67aa4e91c224efac
dglk <dglebovsky@gmail.com>
2016-08-21 16:06:48 +0300

    Added routing module to project Moved user preferences form into a separate template

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 (function() {
   'use strict';
-  angular.module('trainingProject', []);
+  angular.module('trainingProject', ['ui.router']);
+
+  angular.module('trainingProject').config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('main', {
+      url: '/',
+      templateUrl: 'app/templates/main.tpl.html'
+    }).state('main.profile', {
+      url: 'profile',
+      templateUrl: 'app/templates/profile.tpl.html'
+    });
+  });
+
   angular.element(document).ready(function() {
     angular.bootstrap(document, ['trainingProject']);
   });
